2e453d378d8b596f8337d73dc70f27fc77024a41
Hd-Lg <59037834+Hd-Lg@users.noreply.github.com>
2022-11-14 11:21:26 +0100

    Added logic ChatInput

--- app/ChatInput.tsx
+++ app/ChatInput.tsx
@@ -1,5 +1,10 @@
 "use client";
+import { v4 as uuid } from "uuid";
 import { FormEvent, useState } from "react";
+
+import Profile_Pic from "../assets/Profile_Pic.webp";
+import { Message } from "../typings";
+
 type Props = {};
 
 const ChatInput = (props: Props) => {
@@ -11,6 +16,31 @@ const ChatInput = (props: Props) => {
 
 		const messageToSend = input;
 		setInput("");
+		const id = uuid();
+
+		const message: Message = {
+			id,
+			message: messageToSend,
+			created_at: Date.now(),
+			username: "Jean Dupont",
+			// Change the type of profilePic to string
+			profilePic: { Profile_Pic },
+			email: "test@test.com",
+		};
+
+		const uploadMessage = async () => {
+			const res = await fetch("/api/addMessage", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					message,
+				}),
+			});
+
+			const data = await res.json();
+		};
 	};
 	return (
 		<form onSubmit={(e) => addMessage} className="formStyle">
